6744df2b86cb2baf9dc010560f87241149f0b97d
Dustin Davis <dustindavis328@gmail.com>
2024-06-26 14:29:56 -0500

    Password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,20 @@
+class Password {
+  String password = '';
+
+  bool isValid() {
+    bool lengthCheck = password.length >= 8 && password.length <= 16;
+
+    bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
+
+    bool hasLowercase = password.contains(RegExp(r'[a-z]'));
+
+    bool hasNumber = password.contains(RegExp(r'[0-9]'));
+
+    return lengthCheck && hasUppercase && hasLowercase && hasNumber;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}

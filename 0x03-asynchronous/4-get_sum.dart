b1a8fb2bd2c9f4c299b8d21e3a64b39e88cb22c0
Dustin Davis <dustindavis328@gmail.com>
2024-06-26 21:41:58 -0500

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import '4-util.dart';
+import 'dart:convert' as convert;
+
+Future<String> calculateTotal() async {
+  double totalPrice = 0.0;
+  try {
+    var userData = await fetchUserData();
+    Map<String, dynamic> user = convert.jsonDecode(userData);
+    var userId = user['id'];
+
+    var userOrders = await fetchUserOrders(userId);
+    List<dynamic> orders = convert.jsonDecode(userOrders);
+
+    for (var item in orders) {
+      var itemPriceString = await fetchProductPrice(item);
+      var itemPrice = convert.jsonDecode(itemPriceString);
+      totalPrice += itemPrice;
+    }
+
+    return totalPrice.toString();
+  } catch (e) {
+    return '-1';
+  }
+}
